bfa23fac2b6d4d36f83652820ce1ba200f08cf2a
Harry Seong <harryseong@gmail.com>
2023-07-18 16:09:18 -0500

    MUI button use

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Button } from '@mui/material';
 
 function App() {
   return (
@@ -10,14 +11,13 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload. "{process.env.REACT_APP_TITLE} [{process.env.NODE_ENV}]"
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+        <Button variant="text"
+          href='https://reactjs.org'
+          target='_blank'
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   );
