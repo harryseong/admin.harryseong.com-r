0bbb908a33a9e0d2b08a98eeb134d44834a92ef3
Harry Seong <harryseong@gmail.com>
2023-07-17 21:27:48 -0500

    Use process.env

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. "{process.env.REACT_APP_NAME} [{process.env.NODE_ENV}]"
+          Edit <code>src/App.tsx</code> and save to reload. "{process.env.REACT_APP_TITLE} [{process.env.NODE_ENV}]"
         </p>
         <a
           className="App-link"
