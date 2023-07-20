5eb677fdbab7401a603b6863531c2a75fc87cb1a
Harry Seong <harryseong@gmail.com>
2023-07-20 18:45:26 -0500

    Started sidenav

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
+import Grid from '@mui/material/Unstable_Grid2';
 import Navbar from "../components/navbar/Navbar";
+import { List, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from "@mui/material";
 
 
 export function AppLayout() {
@@ -7,9 +9,29 @@ export function AppLayout() {
         <div>
             <Navbar />
 
-            <div>
-                <Outlet />
-            </div>
+            <Grid container spacing={2}>
+                <Grid xs={4} md={3} lg={3}>
+
+                    <List
+                        component='nav'
+                        subheader={<ListSubheader>Pages</ListSubheader>}
+                    >
+                        <ListItemButton>
+                            <ListItemIcon>T</ListItemIcon>
+                            <ListItemText>Places</ListItemText>
+                        </ListItemButton>
+                        <ListItemButton>
+                            <ListItemIcon>A</ListItemIcon>
+                            <ListItemText>About</ListItemText>
+                        </ListItemButton>
+                    </List>
+
+                </Grid>
+                <Grid xs={8} md={9} lg={9}>
+                    <Outlet />
+                </Grid>
+
+            </Grid>
         </div>
-    )
+    );
 }
