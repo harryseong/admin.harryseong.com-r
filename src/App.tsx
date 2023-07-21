ab12038a586fb1c8b750ef2cee09f31e4ffe3187
Harry Seong <harryseong@gmail.com>
2023-07-21 14:05:02 -0500

    Implemented theme

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,34 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { AppLayout } from './layouts/AppLayout';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+export const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AppLayout />}>
-            <Route index element={<Home />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AppLayout />}>
+              <Route index element={<Home />}></Route>
+              <Route path="/login" element={<Login />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
   );
 }
 
