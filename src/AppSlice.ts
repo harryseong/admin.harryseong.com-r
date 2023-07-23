4fd41a293f9f87f11186683efdda29d19ef01f0d
Harry Seong <harryseong@gmail.com>
2023-07-23 16:36:26 -0500

    Create AppSlice

---
+++ src/AppSlice.ts
@@ -0,0 +1,37 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+
+export interface AppState {
+    value: {
+        darkMode: boolean,
+        sidenavOpen: boolean
+    };
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    error: string;
+}
+
+const initialState: AppState = {
+    value: {
+        darkMode: false,
+        sidenavOpen: false
+    },
+    status: 'idle',
+    error: ''
+}
+
+export const appSlice = createSlice({
+    name: 'app',
+    initialState,
+    reducers: {
+        openSidenav: (state, action: PayloadAction<boolean>) => {
+            state.value.sidenavOpen = action.payload;
+        },
+        closeSidenav: (state, action: PayloadAction<boolean>) => {
+            state.value.sidenavOpen = action.payload;
+        },
+    }
+});
+
+export const { openSidenav, closeSidenav } = appSlice.actions;
+
+export default appSlice.reducer;
